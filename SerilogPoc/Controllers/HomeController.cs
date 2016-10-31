d4a51457e9430e4dd3c7cd4202d8ef1df9296bc6
Adam Grande <adam.grande@readify.net>
2016-10-31 13:16:39 +0800

    Use of static logging configuration with Serilog.

---
+++ SerilogPoc/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Serilog;
+
+namespace SerilogPoc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Log.Information("Invoking Index() Action");
+            ViewBag.Title = "Home Page";
+            Log.Information("Finished Index() Action");
+            return View();
+        }
+    }
+}
---
+++ SerilogPoc/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System.Configuration;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Serilog;
+
+namespace SerilogPoc
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            SerilogConfig.Configure();
+            Log.Information("Configuration Application");
+            
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Log.Information("Application Configured");
+        }
+    }
+}
